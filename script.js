b57ff069971f0f85e202d8e57f4ab92a68896d58
Mavludin <imevlud@gmail.com>
2019-10-31 18:51:30 +0300

    Update script.js
    Done

--- script.js
+++ script.js
@@ -1,41 +1,55 @@
-var btn = document.querySelectorAll('div');
-var blocks = document.querySelectorAll('ul');
+$(function() {
 
-var saveHeights = [];
+	var btn = document.querySelectorAll('div');
+	var blocks = document.querySelectorAll('ul');
+	var allArrows = document.querySelectorAll('i');
 
-for (var i=0; i<blocks.length; i++) {
-	blocks[i].id = 'content' + i;
-	saveHeights.push(blocks[i].clientHeight);
-}
+	var saveHeights = [];
 
-for (var i=0; i<blocks.length; i++) {
-	blocks[i].style.height = 0;
-	blocks[i].style.padding = 0;
-}
+	$('ul').each(function(){
+		saveHeights.push($(this).height());
+	});
 
-for (var i=0; i<btn.length; i++) {
-	btn[i].onclick = showContent;
-}
-
-function showContent() {
-
-	var contentElement = this.querySelector('ul');
-
-	if (contentElement.style.padding !== '0px' && contentElement.style.height !== '0px') {
-		contentElement.style.padding = 0;
-		contentElement.style.height = 0;
+	for (var i=0; i<blocks.length; i++) {
+		blocks[i].id = 'content' + i;
 	}
 
-	else {
+	for (var i=0; i<blocks.length; i++) {
+		blocks[i].style.height = 0;
+		blocks[i].style.padding = 0;	
+	}
 
-		for (var i=0; i<btn.length; i++) {
-			blocks[i].style.height = 0;
-			blocks[i].style.padding = 0;
+	for (var i=0; i<btn.length; i++) {
+		btn[i].querySelector('span').onclick = showContent;
+	}
+
+	function showContent() {
+
+		var contentElement = this.parentNode.querySelector('ul');
+		var currentArrow = this.querySelector('i');
+
+
+		if (contentElement.style.height !== '0px') {
+			contentElement.style.height = 0;
+			contentElement.style.padding = 0;
+			currentArrow.style.transform = 'none';
 		}
 
-		var currentHeight = contentElement.id.substr(7);
-		contentElement.style.padding = '5px';
-		contentElement.style.height = saveHeights[currentHeight] + saveHeights[currentHeight] * 0.05  + 'px';
+		else {
+
+			for (var i=0; i<btn.length; i++) {
+				blocks[i].style.height = 0;
+				blocks[i].style.padding = 0;
+				allArrows[i].style.transform = 'none';
+			}
+
+			currentArrow.style.transform = 'rotate(180deg)';
+
+			var currentHeight = contentElement.id.substr(7);
+			contentElement.style.height = saveHeights[currentHeight] + 'px';
+			contentElement.style.padding = '5px 10px';
+		}
+
 	}
 
-}
+});
